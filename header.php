30d9402f07e5cb3c8db465115fed3a3f613011c6
Hakan Çalış <80002994+hakancalis@users.noreply.github.com>
2021-11-08 20:57:00 +0300

    Update header.php

--- header.php
+++ header.php
@@ -6,7 +6,7 @@ require_once 'DatabaseClass/database.class.php';
 <html lang="en">
 
 <head>
-    <base href="http://localhost/kisalink/">
+    <base href="<?=$link?>">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
